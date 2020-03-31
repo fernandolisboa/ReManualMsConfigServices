cc66331b998ccd4032f1ccefcd0b1a08389c8e61
Fernando <fernandoigorlisboa@gmail.com>
2020-03-31 17:10:52 -0300

    A bunch of crap

--- ReManualMsConfigServices/Program.cs
+++ ReManualMsConfigServices/Program.cs
@@ -1,17 +1,36 @@
-﻿using Microsoft.Win32;
+﻿//using System.Text.RegularExpressions;
+//public static readonly Regex UserRegex = new Regex(@"^.+_\w+$");
+//var regexSvcs = AllServices.Where(s => UserRegex.IsMatch(s.ServiceName));
+//PrintServices(regexSvcs, "UserRegex");
+
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Management;
 using System.ServiceProcess;
+using System.Text;
+
+using static System.StringComparer;
 
 namespace ReManualMsConfigServices
 {
     public class Program
     {
+        public static string USER_LUID = string.Empty;
+
         #region props
 
-        private static readonly Dictionary<char, bool> KeyOptions = new Dictionary<char, bool>
+        #region user
+
+        public static bool UserServicesPredicate(ServiceController s) => s.ServiceName.Contains(USER_LUID, StringComparison.OrdinalIgnoreCase);
+
+        public static IEnumerable<string> UserServices = null;
+
+        #endregion user
+
+        public static readonly Dictionary<char, bool> KeyOptions = new Dictionary<char, bool>
         {
             { 's', true },
             { 'S', true },
@@ -19,42 +38,66 @@ namespace ReManualMsConfigServices
             { 'N', false },
         };
 
-        private static ServiceController[] AllServices = null;
-        private static readonly IEnumerable<string> SvcsMustStayDisabled = null;
+        public static IEnumerable<ServiceController> AllServices = null;
+
+        public const string AllServicesFilePath = @".\config\AllServices.csv";
+
+        public const string DontTouchServicesFilePath = @".\config\DontTouchServices.txt";
+        public static IEnumerable<string> DontTouchServices = null;
+
+        public const string DisabledServicesFilePath = @".\config\DisabledServices.txt";
+        public static IEnumerable<string> DisabledServices = null;
 
         #endregion props
-
-        private static void RefreshAllServices() => AllServices = ServiceController.GetServices();
+        #region core
 
         static Program()
         {
-            SvcsMustStayDisabled = File.ReadAllLines("SvcsMustStayDisabled.txt");
-
             RefreshAllServices();
+            RefreshDisabledServices();
         }
 
         public static void Main()
         {
-            //ManualServices();
+            HandleConfig();
+            WriteAllServicesFile();
 
-            Print39b92Services();
+            DisableServices();
+            ManualServices();
 
-            PrintDisabledServices();
-
-            Console.Write("Fim da execução. Pressione qualquer tecla para finalizar...");
+            Console.Write("\n\n\n### Fim da execução. Pressione qualquer tecla para finalizar... ###");
             Console.ReadKey();
         }
 
+        #endregion core
+        #region util
+
+        public static bool GetKeyOption(bool defaultOption = false)
+        {
+            var pressedKey = Console.ReadKey();
+            Console.WriteLine("\n");
+
+            if(pressedKey.Key == ConsoleKey.Enter)
+            {
+                return defaultOption;
+            }
+
+            KeyOptions.TryGetValue(pressedKey.KeyChar, out var option);
+
+            return option;
+        }
+
+        #endregion util
         #region change services
 
         public static void ManualServices()
         {
-            ChangeServicesStartType(s => !SvcsMustStayDisabled.Contains(s.ServiceName) && s.StartType == ServiceStartMode.Disabled);
+            ChangeServicesStartType(s => !DisabledServices.Contains(s.ServiceName, OrdinalIgnoreCase) && s.StartType == ServiceStartMode.Disabled);
         }
 
         public static void DisableServices()
         {
-            var disableServices = AllServices.Where(s => SvcsMustStayDisabled.Contains(s.ServiceName) && s.StartType != ServiceStartMode.Disabled);
+            var disableServices = AllServices.Where(s => DisabledServices.Contains(s.ServiceName, OrdinalIgnoreCase) && s.StartType != ServiceStartMode.Disabled);
             ChangeServicesStartType(disableServices, ServiceStartMode.Disabled);
 
             PrintDisabledServices();
@@ -63,7 +106,7 @@ namespace ReManualMsConfigServices
         public static void ChangeServicesStartType(Func<ServiceController, bool> predicate, ServiceStartMode startType = ServiceStartMode.Manual)
         {
             var services = AllServices.Where(predicate);
-            ChangeServicesStartType(services);
+            ChangeServicesStartType(services, startType);
         }
 
         public static void ChangeServicesStartType(IEnumerable<ServiceController> services, ServiceStartMode startType = ServiceStartMode.Manual)
@@ -75,7 +118,7 @@ namespace ReManualMsConfigServices
             {
                 foreach (var service in services)
                 {
-                    ChangeServiceStartType(service.ServiceName);
+                    ChangeServiceStartType(service.ServiceName, startType);
                 }
 
                 RefreshAllServices();
@@ -90,35 +133,132 @@ namespace ReManualMsConfigServices
             {
                 var sc = new ServiceController(serviceName);
 
+                if (sc.CanStop)
+                {
+                    sc.Stop();
+                }
+
                 var key = Registry.LocalMachine.OpenSubKey($@"SYSTEM\CurrentControlSet\Services\{sc.ServiceName}", true);
                 key.SetValue("Start", (int)startType);
+
+                Console.WriteLine($"Serviço '{serviceName}' alterado para '{startType}'");
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Erro mudando o serviço '{serviceName}' para '{startType}'\n\n[[[\n\n{e}\n\n]]]\n");
             }
-
-            Console.WriteLine($"Serviço '{serviceName}' alterado para '{startType}'");
         }
 
         #endregion change services
-        #region util
+        #region services.msc
 
-        public static bool GetKeyOption()
+        public static void WriteAllServicesFile()
         {
-            var pressedKey = Console.ReadKey();
-            Console.WriteLine("\n");
+            var orderedServices = AllServices.Select(s => $"{s.DisplayName};{s.ServiceName};{s.StartType};{s.ServiceType};\"{s.GetServiceDescription()}\"")
+                                             .OrderBy(s => s, OrdinalIgnoreCase);
 
-            KeyOptions.TryGetValue(pressedKey.KeyChar, out var option);
+            var servicesHeader = new List<string> { "DisplayName;ServiceName;StartType;ServiceType;Description" };
+            servicesHeader.AddRange(orderedServices);
 
-            return option;
+            File.WriteAllLines(AllServicesFilePath, servicesHeader, Encoding.UTF8);
         }
 
+        public static void RefreshAllServices() => AllServices = ServiceController.GetServices();
+
+        public static void RefreshUserServices() => UserServices = AllServices.Where(UserServicesPredicate).Select(s => s.ServiceName);
+
+        #endregion services.msc
+        #region config
+
+        private static void HandleConfig()
+        {
+            HandleConfigUserServices();
+
+            RemoveInvalidServices();
+
+            RefreshDontTouchServices();
+            RemoveDontTouchServices();
+        }
+
+        public static void RefreshDontTouchServices()
+        {
+            var rawServices = File.ReadAllLines(DontTouchServicesFilePath);
+            DontTouchServices = rawServices.Select(s => string.Format(s, USER_LUID));
+        }
+        public static void RefreshDisabledServices() => DisabledServices = File.ReadAllLines(DisabledServicesFilePath);
+
+        public static void UpdateAndRefreshDisabledServicesFile(IEnumerable<string> newDisabledServices)
+        {
+            var orderedNewDisabledServices = newDisabledServices.OrderBy(s => s, OrdinalIgnoreCase);
+
+            File.WriteAllLines(DisabledServicesFilePath, orderedNewDisabledServices, Encoding.UTF8);
+            RefreshDisabledServices();
+        }
+
+        public static void AppendDisabledServices(string service) => AppendDisabledServices(new List<string>() { service });
+        public static void AppendDisabledServices(IEnumerable<string> services)
+        {
+            var newDisabledServices = DisabledServices.Concat(services);
+            UpdateAndRefreshDisabledServicesFile(newDisabledServices);
+        }
+
+        public static void RemoveDisabledServices(string service) => RemoveDisabledServices(new List<string>() { service });
+        public static void RemoveDisabledServices(IEnumerable<string> services)
+        {
+            //var newDisabledServices = DisabledServices.Where(s => !services.Contains(s, OrdinalIgnoreCase));
+            var newDisabledServices = DisabledServices.Except(services, OrdinalIgnoreCase);
+            UpdateAndRefreshDisabledServicesFile(newDisabledServices);
+        }
+
+        public static void RemoveInvalidServices()
+        {
+            var allServices = AllServices.Select(s => s.ServiceName);
+            var invalidServices = DisabledServices.Except(allServices, OrdinalIgnoreCase);
+
+            RemoveDisabledServices(invalidServices);
+        }
+
+        public static void RemoveDontTouchServices()
+        {
+            var allServices = AllServices.Select(s => s.ServiceName);
+            var dontTouchServices = DontTouchServices.Except(allServices, OrdinalIgnoreCase);
+
+            RemoveDisabledServices(dontTouchServices);
+        }
+
+        #region user services
+
+        private static void RequestUserLUID()
+        {
+            Console.Write("Deseja informar o LUID do usuário? [S / n] " );
+            var willTypeLUID = GetKeyOption(true);
+
+            while (willTypeLUID && string.IsNullOrWhiteSpace(USER_LUID))
+            {
+                Console.Write("LUID (ver em services.msc): ");
+                USER_LUID = Console.ReadLine();
+            }
+
+            Console.WriteLine("\n");
+        }
+
+        public static void AppendUserServices() => AppendDisabledServices(UserServices);
+
+        private static void HandleConfigUserServices()
+        {
+            RequestUserLUID();
+            RefreshUserServices();
+            AppendUserServices();
+        }
+
+        #endregion user services
+
+        #endregion config
         #region print services
 
         public static bool AskToPrint(string tag = "em extenso")
         {
-            Console.Write($"Deseja imprimir os serviços '{tag}'? [s / N] ");
+            Console.Write($"\nDeseja imprimir os serviços '{tag}'? [s / N] ");
 
             var print = GetKeyOption();
             // Console.WriteLine("Opção inválida! Os serviços não serão imprimidos...");
@@ -135,7 +275,8 @@ namespace ReManualMsConfigServices
             return print;
         }
 
-        public static void PrintService(ServiceController service) => Console.WriteLine($@"{service.DisplayName} ({service.ServiceName}) [{service.Status}]");
+        //public static void PrintService(ServiceController service) => Console.WriteLine($@"{service.DisplayName} ({service.ServiceName}) [{service.Status}]");
+        public static void PrintService(ServiceController service) => Console.WriteLine(service.ServiceName);
         public static void PrintServices(IEnumerable<ServiceController> services, string tag = "padrão")
         {
             var printEachService = AskToPrintEachService();
@@ -144,6 +285,8 @@ namespace ReManualMsConfigServices
 
             if (printEachService)
             {
+                Console.WriteLine($"\nINI '{tag}'\n");
+
                 foreach (var service in services)
                 {
                     PrintService(service);
@@ -163,14 +306,31 @@ namespace ReManualMsConfigServices
             }
         }
 
-        public static void PrintUserServices(string userHash) => PrintServices(s => s.ServiceName.Contains(userHash), userHash);
+        public static void PrintUserServices() => PrintServices(UserServicesPredicate, USER_LUID);
 
-        public static void Print39b92Services() => PrintUserServices("39b92"); // DELL G3
-        public static void Print1d6ab79Services() => PrintUserServices("1d6ab79"); // DESKTOP BSB
         public static void PrintDisabledServices() => PrintServices(s => s.StartType == ServiceStartMode.Disabled, "desabilitados");
 
         #endregion print services
+    }
 
-        #endregion util
+    public static class Extensions
+    {
+        public static string GetServiceDescription(this ServiceController service)
+        {
+            var managementPath = new ManagementPath($"Win32_Service.Name='{service.ServiceName}'");
+            using var managementObject = new ManagementObject(managementPath);
+
+            var description = "Sem descrição";
+            try
+            {
+                description = managementObject["Description"]?.ToString();
+            }
+            catch (Exception ex)
+            {
+                description += $" [{ex.Message}]";
+            }
+
+            return description;
+        }
     }
 }
